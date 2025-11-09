ca26eda791851dc5769edc29135068d43fc5664b
vladoja <dr.jalo2@gmail.com>
2025-11-09 23:26:19 +0100

    L9 Model, migration for Ninja table/model.

---
+++ application/app/Models/Ninja.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ninja extends Model
+{
+    /** @use HasFactory<\Database\Factories\NinjaFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'skill',
+        'bio',
+    ];
+}
