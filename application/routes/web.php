4b0fe77afe1784cdd753c8c08d203fb739e3bffe
vladoja <dr.jalo2@gmail.com>
2025-11-09 19:31:57 +0100

    L7 Setting up CSS.

--- application/routes/web.php
+++ application/routes/web.php
@@ -11,7 +11,7 @@ Route::get('/hello', fn () => 'Hello from Docker!');
 Route::get('/ninjas', function () {
     $ninjas = [
         ["name" => "Yoshi","skill" => 75, "age" => 25, "id" => 1],
-        ["name" => "Kuma", "skill" => 80, "age" => 30, "id" => 2],
+        ["name" => "Kuma", "skill" => 45, "age" => 30, "id" => 2],
         ["name" => "Hattori", "skill" => 90, "age" => 28, "id" => 3 ],
     ];
     return view('ninjas.index', [ "greeting" => "Hello Ninjas!", "ninjas" => $ninjas]);
