557d6298c189534f6d2e97f825d0b6caede8e35f
vladoja <dr.jalo2@gmail.com>
2025-11-10 23:08:03 +0100

    L16 Doja. Migration,Factory,Seeder.

--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $this->call([NinjaSeeder::class]);
+        $this->call([
+            DojoSeeder::class,
+            NinjaSeeder::class]);
     }
 }
