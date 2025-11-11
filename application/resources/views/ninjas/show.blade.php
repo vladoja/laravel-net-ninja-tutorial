3d2ffe2aafe9f420f6523a395a09d486109a54c1
vladoja <dr.jalo2@gmail.com>
2025-11-11 09:44:33 +0100

    L17 Showing dojo details in Ninja show.

--- application/resources/views/ninjas/show.blade.php
+++ application/resources/views/ninjas/show.blade.php
@@ -6,4 +6,15 @@
         <p><strong>About me:</strong></p>
         <p>{{ $ninja['bio'] }}</p>
     </div>
+
+    <div class="border-2 border-dashed bg-white px-4 pb-4 rounded">
+        <h3 class="text-lg font-semibold mt-4 mb-2">Dojo Information</h3>
+        @if ($ninja->dojo)
+            <p><strong>Name:</strong> {{ $ninja->dojo->name }}</p>
+            <p><strong>Location:</strong> {{ $ninja->dojo->location }}</p>
+            <p><strong>Description:</strong> {{ $ninja->dojo->description }}</p>
+        @else
+            <p>This ninja is not affiliated with any dojo.</p>
+        @endif
+    </div>
 </x-layout>
