d532bd1950f5df105e1090fd3dd2b8b764570fd4
vladoja <dr.jalo2@gmail.com>
2025-11-13 00:15:09 +0100

    LAT - L3 Register and login views.

--- application/resources/views/auth/login.blade.php
+++ application/resources/views/auth/login.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-    <h2>Login</h2>
+    <form action="">
+        @csrf
+        <h2>Log in to your account</h2>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required value="{{ old('email') }}">
+
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+
+        <button class="btn mt-4">Log in</button>
+    </form>
 </x-layout>
--- application/resources/views/auth/register.blade.php
+++ application/resources/views/auth/register.blade.php
@@ -1,3 +1,19 @@
 <x-layout>
-    <h2>Register</h2>
+    <form action="">
+        @csrf
+        <h2>Create a new account</h2>
+        <label for="name">Name</label>
+        <input type="text" name="name" required value="{{ old('name') }}">
+
+        <label for="email">Email</label>
+        <input type="email" name="email" required value="{{ old('email') }}">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" required>
+
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" name="password_confirmation" required>
+
+        <button class="btn mt-4">Register</button>
+    </form>
 </x-layout>
