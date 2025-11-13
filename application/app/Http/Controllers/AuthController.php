fc4a0da5d332608ff837e1261b25209e88c977e0
vladoja <dr.jalo2@gmail.com>
2025-11-13 10:14:18 +0100

    LAT - L4 Creating new user. Showing form errors.

--- application/app/Http/Controllers/AuthController.php
+++ application/app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
@@ -19,7 +22,22 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        // Registration logic here
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            // password_confirmation field is automatically validated by 'confirmed' rule
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => $validated['password']
+            // Note: In a real application, ensure to hash the password before storing it
+            // 'password' => bcrypt($validated['password']        ),
+        ]);
+        Auth::login($user);
+        return redirect()->route('ninjas.index');
     }
 
     public function login(Request $request)
--- application/resources/views/auth/register.blade.php
+++ application/resources/views/auth/register.blade.php
@@ -15,5 +15,13 @@
         <input type="password" name="password_confirmation" required>
 
         <button class="btn mt-4">Register</button>
+
+        @if ($errors->any())
+            <ul class="px-4 py-2 bg-red-100">
+                @foreach ($errors->all() as $error)
+                    <li class="my-2 text-red-500">{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
     </form>
 </x-layout>
