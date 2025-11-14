e070a7ec65b75d106e895bf3abea2dacb2ab2c96
vladoja <dr.jalo2@gmail.com>
2025-11-14 19:34:11 +0100

    LAT - L8 Protecting routes for authenticated users.

--- application/routes/web.php
+++ application/routes/web.php
@@ -9,14 +9,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/hello', fn() => 'Hello from Docker!');
 
 Route::middleware('auth')->group(function () {
     Route::get('/ninjas', [NinjaController::class, 'index'])->name('ninjas.index');
